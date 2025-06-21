52f76f311a741717d41cf79155f32deadae8a39e
kanishk-singh19 <23bcs046@nith.ac.in>
2025-06-21 05:48:28 +0530

    implemented hero scroll animation

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
     <main>
       <Navbar />  
       <Hero />
+      
     </main>
   )
 }
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,6 +1,71 @@
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { SplitText } from 'gsap/all'
+import { useRef } from 'react'
+import { useMediaQuery } from 'react-responsive'
+
 import React from 'react'
+// import { useMediaQuery } from 'react-responsive'
 
 const Hero = () => {
+  const videoRef = useRef();
+  const isMobile = useMediaQuery({maxWidth:767})
+
+  useGSAP(()=>{
+    const heroSplit = new SplitText('.title', {type:'chars,words'})
+    const paragraphSplit = new SplitText('.subtitle', {type:'lines'})
+
+    heroSplit.chars.forEach((char) => char.classList.add('text-gradient'))
+    gsap.from(heroSplit.chars, {
+      yPercent:100,
+      duration:1.8,
+      ease:'expo.out',
+      stagger:0.05
+    })
+    gsap.from(paragraphSplit.lines, {
+      opacity:0,
+      yPercent:100,
+      duration:1.8,
+      ease:'expo.out',
+      stagger:0.06,
+      delay:1,
+    });
+
+    gsap.timeline({
+      scrollTrigger:{
+        trigger:'#hero',
+        start:'top top',
+        end:'bottom top',
+        scrub:true,
+        
+      }
+    })
+    .to('.right-leaf', {y:200},0)
+    .to('left-leaf',{y:-200},0)
+    
+    const startValue = isMobile ? 'top 50%':'center 60%';
+    const endValue = isMobile ? '120% top':'bottom top';
+
+    const tl = gsap.timeline({
+      scrollTrigger:{
+        trigger:'video',
+        start:startValue,
+        end:endValue,
+        scrub:true,
+        pin:true,
+        pinSpacing:false, 
+      }
+    })
+    
+    videoRef.current.onloadedmetadata =() => {
+      tl.to(videoRef.current,{
+        currentTime : videoRef.current.duration
+      })
+    }
+    
+  },[]);
+
+
   return (
     <>
     <section id='hero' className='noisy'>
@@ -19,9 +84,28 @@ const Hero = () => {
                 Sip the Spirit <br/> of Summer
               </p>
             </div>
+
+            <div className='view-cocktails'>
+              <p className='subtitle'>
+                Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes - designed to delight your senses.
+              </p>
+              <a href="#cocktails">View Cocktails</a>
+
+            </div>
           </div>
         </div>
     </section>
+    <div className='video absolute inset-0'>
+    <video 
+    ref={videoRef}
+    
+    muted
+    playsInline
+    preload='auto'
+    src='/videos/output.mp4'
+   />
+    </div>
+
     </>
   )
 }
